122402d9871c1a1c765fd6b4584984465a925eef
gameiro09 <2141218@my.ipleiria.pt>
2016-10-24 21:41:15 +0100

    teste
    teste

---
+++ WcfServiceCalculator/Service1.svc.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace WcfServiceCalculator
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in code, svc and config file together.
+    // NOTE: In order to launch WCF Test Client for testing this service, please select Service1.svc or Service1.svc.cs at the Solution Explorer and start debugging.
+    public class Service1 : IService1
+    {
+        public float Divide(float a, float b)
+        {
+            if(b == 0)
+            {
+                throw new Exception("'b' cannot be null");
+
+            } else
+            {
+                return a / b;
+            }
+        }
+
+        public string GetData(int value)
+        {
+            return string.Format("You entered: {0}", value);
+        }
+
+        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        {
+            if (composite == null)
+            {
+                throw new ArgumentNullException("composite");
+            }
+            if (composite.BoolValue)
+            {
+                composite.StringValue += "Suffix";
+            }
+            return composite;
+        }
+
+        public float Multiply(float a, float b)
+        {
+            return a * b;
+        }
+
+        public float Subtract(float a, float b)
+        {
+            return a - b;
+        }
+
+        public float Sum(float a, float b)
+        {
+            return a + b;
+        }
+    }
+}
